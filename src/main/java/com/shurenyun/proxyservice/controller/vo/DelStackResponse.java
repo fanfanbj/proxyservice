c8ba26c9e268810f041d1d1fbf70d757d918cc91
fan bin <bfan@dataman-inc.com>
2016-06-28 04:01:02 +0800

    minor checkin

--- src/main/java/com/shurenyun/proxyservice/controller/vo/DelStackResponse.java
+++ src/main/java/com/shurenyun/proxyservice/controller/vo/DelStackResponse.java
@@ -1,5 +1,23 @@
 package com.shurenyun.proxyservice.controller.vo;
 
 public class DelStackResponse {
+	
+	String status;
+	String error_message;
+	
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getError_message() {
+		return error_message;
+	}
+	public void setError_message(String error_message) {
+		this.error_message = error_message;
+	}
+	
+	
 
 }
